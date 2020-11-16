5e9e70dce467f9a41c68473fb03ce0aaf8ac7bdb
Ashir <ashirkul@gmail.com>
2020-11-16 17:25:03 +0530

    #8-encrypted signup signin

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,10 +2,8 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
+const bcrypt = require('bcryptjs')
 
-router.get('/',(req,res)=>{
-    res.send("hello")
-})
 
 router.post('/signup',(req,res)=>{
     const {name,email,password} = req.body;
@@ -17,16 +15,46 @@ router.post('/signup',(req,res)=>{
         if(savedUser){
             return res.status(422).json({error : "User already exist"});
         }
-        const user = new User({email,password,name})
-        user.save().then(user=>{
-            res.json({message : "Signup successful"});
-        }).catch(err=>{
-            console.log(err);
+        bcrypt.hash(password,12).then(hashedpassword=>{
+
+            const user = new User({
+                email,
+                password:hashedpassword,
+                name})
+            user.save().then(user=>{
+                res.json({message : "Signup successful"});
+            }).catch(err=>{
+                console.log(err);
+            })
         })
+
     }).catch(err=>{
         console.log(err);
     })
     
 })
 
+
+router.post('/signin',(req,res)=>{
+    const {email,password} = req.body
+    if(!email || !password){
+        return res.status(422).json({error :"Invalid email or password"})
+    }
+
+    User.findOne({email:email}).then(savedUser=>{
+        if(!savedUser){
+            return res.status(422).json({error :"Invalid email or password"})
+        }
+
+        bcrypt.compare(password,savedUser.password).then(doMatch=>{
+            if(!doMatch){
+                return res.status(422).json({error :"Invalid email or password"})
+            }
+
+            res.json({message:"Successfully signed in"})
+        }).catch(err=>{
+            console.log(err)
+        })
+    })
+})
 module.exports = router
