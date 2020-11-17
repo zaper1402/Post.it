09f45863949689c664982091d32c462f0e9a8552
Ashir <ashirkul@gmail.com>
2020-11-17 19:11:24 +0530

    #10 JWT token auth

--- Backend/app.js
+++ Backend/app.js
@@ -4,9 +4,7 @@ const PORT = 5000;
 const mongoose = require('mongoose')
 const {MONGOURI}  = require('./keys')
 
-require('./models/user')
-app.use(express.json())
-app.use(require('./routes/auth'))
+
 mongoose.connect(MONGOURI,{
     useNewUrlParser: true,
     useUnifiedTopology:true
@@ -17,6 +15,12 @@ mongoose.connection.on('connected',()=>{
 mongoose.connection.on('error',(err)=>{
     console.log("err connecting",err);
 })
+
+require('./models/user')
+
+
+app.use(express.json())
+app.use(require('./routes/auth'))
 
 app.listen(PORT,()=>{
     console.log("server is running on",PORT)
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -3,7 +3,13 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../keys')
+const requireLogin = require('../middlewares/requireLogin')
 
+router.get('/protected',requireLogin,(req,res)=>{
+    res.send("hello user")
+})
 
 router.post('/signup',(req,res)=>{
     const {name,email,password} = req.body;
@@ -47,14 +53,16 @@ router.post('/signin',(req,res)=>{
         }
 
         bcrypt.compare(password,savedUser.password).then(doMatch=>{
-            if(!doMatch){
+            if(!doMatch){ 
                 return res.status(422).json({error :"Invalid email or password"})
             }
-
-            res.json({message:"Successfully signed in"})
+            const token = jwt.sign({_id:savedUser._id},JWT_SECRET)
+            res.json({token})
         }).catch(err=>{
             console.log(err)
         })
     })
 })
+
+
 module.exports = router
