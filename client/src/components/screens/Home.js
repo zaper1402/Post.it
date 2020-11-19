184d503eacbc49ec49e6e147925b98d685d4d2ad
Ashir <ashirkul@gmail.com>
2020-11-20 01:04:37 +0530

    #25 fetch post(home screen)

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,69 +1,41 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 const Home = ()=>{
+    const [data,setData] = useState([])
+    useEffect(()=>{
+        fetch('/allpost',{
+            headers:{
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            }
+        }).then(res=>res.json())
+        .then(result=>{
+            // console.log(result)
+            setData(result.posts);
+        })
+    },[])
+
     return (
         <div className="home">
-            <div className="card home-card">
-                <h5>Stephen</h5>
-                <div className="card-image">
-                    <img src="https://images.unsplash.com/photo-1446941303752-a64bb1048d54?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"/>
-                </div>
-                <div className="card-content">
-                    <i className="material-icons">favorite</i>
-                    <h6>title</h6>
-                    <p>this is amazing post</p>
-                    <input type="text" placeholder="add a comment"/>
-                </div>
-            </div>
+            {
+                data.map(item=>{
+                    return (
+                        <div className="card home-card" key={item._id}>
+                            <h5>{item.postedBy.name}</h5>
+                            <div className="card-image">
+                                <img src={item.photo}/>
+                            </div>
+                            <div className="card-content">
+                                <i className="material-icons">favorite</i>
+                                <h6>{item.title}</h6>
+                                <p>{item.body}</p>
+                                <input type="text" placeholder="add a comment"/>
+                            </div>
+                        </div>
+                    )
+                })
+            }
 
-            <div className="card home-card">
-                <h5>Stephen</h5>
-                <div className="card-image">
-                    <img src="https://images.unsplash.com/photo-1446941303752-a64bb1048d54?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"/>
-                </div>
-                <div className="card-content">
-                    <i className="material-icons">favorite</i>
-                    <h6>title</h6>
-                    <p>this is amazing post</p>
-                    <input type="text" placeholder="add a comment"/>
-                </div>
-            </div>
 
-            <div className="card home-card">
-                <h5>Stephen</h5>
-                <div className="card-image">
-                    <img src="https://images.unsplash.com/photo-1446941303752-a64bb1048d54?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"/>
-                </div>
-                <div className="card-content">
-                    <h6>title</h6>
-                    <p>this is amazing post</p>
-                    <input type="text" placeholder="add a comment"/>
-                </div>
-            </div>
-
-            <div className="card home-card">
-                <h5>Stephen</h5>
-                <div className="card-image">
-                    <img src="https://images.unsplash.com/photo-1446941303752-a64bb1048d54?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"/>
-                </div>
-                <div className="card-content">
-                    <h6>title</h6>
-                    <p>this is amazing post</p>
-                    <input type="text" placeholder="add a comment"/>
-                </div>
-            </div>
-
-            <div className="card home-card">
-                <h5>Stephen</h5>
-                <div className="card-image">
-                    <img src="https://images.unsplash.com/photo-1446941303752-a64bb1048d54?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"/>
-                </div>
-                <div className="card-content">
-                    <h6>title</h6>
-                    <p>this is amazing post</p>
-                    <input type="text" placeholder="add a comment"/>
-                </div>
-            </div>
         </div>
     )
 }
