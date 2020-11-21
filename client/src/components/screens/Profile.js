459b438e935d1c5c125f06ccfc106859f7d5d9ab
Ashir <ashirkul@gmail.com>
2020-11-21 13:41:09 +0530

    #27 Dynamic profile page

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React,{useEffect,useState,useContext} from 'react'
+import {UserContext} from '../../App'
 
 const Profile = ()=>{
+    const [mypics,setpics] = useState([])
+    const {state,dispatch} = useContext(UserContext)
+    useEffect(()=>{
+        fetch('/myposts',{
+            headers:{
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            }
+        }).then(res=>res.json())
+        .then(result=>{
+            setpics(result.mypost)
+        })
+    },[])
     return (
         <div style={{maxWidth:"1150px" ,margin:"0px auto"}}>
             <div style={{
@@ -14,7 +27,7 @@ const Profile = ()=>{
                     src="https://images.unsplash.com/photo-1493752603190-08d8b5d1781d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
                 </div>
                 <div>
-                    <h4>Stephen Dent</h4>
+                    <h4>{state?state.name:"loading..."}</h4>
                     <div style = {{display:"flex", justifyContent:"space-between",width:"108%"}}>
                         <h6>40 Posts</h6>
                         <h6>40 Followers</h6>
@@ -24,15 +37,14 @@ const Profile = ()=>{
             </div>
             
             <div className="gallery" style={{}}>
-                <img className="item" src="https://images.unsplash.com/photo-1493752603190-08d8b5d1781d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
-                <img className="item" src="https://images.unsplash.com/photo-1493752603190-08d8b5d1781d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
-                <img className="item" src="https://images.unsplash.com/photo-1493752603190-08d8b5d1781d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
-                <img className="item" src="https://images.unsplash.com/photo-1493752603190-08d8b5d1781d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
-                <img className="item" src="https://images.unsplash.com/photo-1493752603190-08d8b5d1781d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
-                <img className="item" src="https://images.unsplash.com/photo-1493752603190-08d8b5d1781d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
-                <img className="item" src="https://images.unsplash.com/photo-1493752603190-08d8b5d1781d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
-                <img className="item" src="https://images.unsplash.com/photo-1493752603190-08d8b5d1781d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
-                <img className="item" src="https://images.unsplash.com/photo-1493752603190-08d8b5d1781d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+                {
+                    mypics.map(item=>{
+                        return(
+                            <img className="item" src={item.photo} alt={item.title}/>
+                        )
+                    })
+                }
+               
             </div>
         </div>
     )
