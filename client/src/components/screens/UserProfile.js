a8cd5832cb45ed27d10812105f8898579282753d
Ashir <ashirkul@gmail.com>
2020-11-24 16:15:53 +0530

    #36 follow unfollow

--- client/src/components/screens/UserProfile.js
+++ client/src/components/screens/UserProfile.js
@@ -8,16 +8,47 @@ const Profile = () => {
     const { userid } = useParams()
     // console.log(userid);
     useEffect(() => {
-        fetch(`/user/${userid}`, {
+        fetch(`/user/${userid}`,{
             headers: {
                 "Authorization": "Bearer " + localStorage.getItem("jwt")
             }
         }).then(res => res.json())
             .then(result => {
-                console.log(result);
+                // console.log(result);
                 setProfile(result)
             })
     }, [])
+
+    const followUser = ()=>{
+        fetch('/follow',{
+            method:"put",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization": "Bearer " + localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                followId:userid
+            })
+        }).then(res=>res.json())
+        .then(data=>{
+            dispatch({type:"UPDATE",payload:{following:data.following,followers:data.followers}})
+            localStorage.setItem("user",JSON.stringify(data))
+            // console.log(data);
+            setProfile((prevState)=>{
+                return {
+                    ...prevState,
+                    user:data
+                }
+            })
+        })
+    }
+
+
+
+
+
+
+
     return (
         <>
             {userProfile ?
@@ -37,9 +68,14 @@ const Profile = () => {
                             <h5>{userProfile.user.email}</h5>
                             <div style={{ display: "flex", justifyContent: "space-between", width: "108%" }}>
                                 <h6>{userProfile.posts.length} Posts</h6>
-                                <h6>40 Followers</h6>
-                                <h6>40 Following</h6>
+                                <h6>{userProfile.user.followers.length} Followers</h6>
+                                <h6>{userProfile.user.following.length} Following</h6>
                             </div>
+                            <button className="btn waves-effect waves-light #4fc3f7 light-blue lighten-2"
+                            onClick={()=>followUser()}
+                            >
+                                Follow
+                            </button>
                         </div>
                     </div>
 
