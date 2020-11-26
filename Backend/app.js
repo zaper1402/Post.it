ea86980604b15ca3be7773cbe2cd49080aa2564d
Ashir <ashirkul@gmail.com>
2020-11-26 21:16:59 +0530

    Heroku deploy

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose')
-const {MONGOURI}  = require('./keys')
+const {MONGOURI}  = require('./config/keys')
 
 
 mongoose.connect(MONGOURI,{
@@ -23,6 +23,16 @@ app.use(express.json())
 app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
 app.use(require('./routes/user'))
+
+
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('../client/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+
 
 app.listen(PORT,()=>{
     console.log("server is running on",PORT)
